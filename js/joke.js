48a184f555b346624576ce9c8b2ea4c7f0254a94
Spiky Hair JJ <wenjay.ding@gmail.com>
2025-06-09 09:48:00 +0800

    added a few folders

---
+++ js/joke.js
@@ -0,0 +1,10 @@
+// SOOOOOOO.....
+// Too any ppl who knows how to code
+// i found this joke.....
+"American English"
+let favoriteColor = "red";
+console.log(`Favorite Color $${favoriteColor}`);
+
+"British English"
+let favouriteColour = "red";
+console.log(`Favourite Colour £${favouriteColour}`);
