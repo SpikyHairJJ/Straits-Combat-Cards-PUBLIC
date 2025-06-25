e4a3933f9c6f60b3d2239aed3c18c342e778664d
Spiky Hair JJ <wenjay.ding@gmail.com>
2025-06-25 19:09:52 +0800

    more stuff

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, jsonify, render_template
-import #tbc
+import mysql.connector 
+from mysql.connector import Error
 
 app = Flask(__name__, template_folder='templates')
 
 @app.route('/')
 def home() :
+    print("Rendering")
     return render_template('index.html')
-
-@app.route()
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
