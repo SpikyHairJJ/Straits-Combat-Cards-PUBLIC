8770787905dbdcc9ddcbc1153fff41e7d448e7c9
Spiky Hair JJ <wenjay.ding@gmail.com>
2025-07-08 20:59:54 +0800

    halfway login deisgn

---
+++ static/js/login.js
@@ -0,0 +1,27 @@
+let welcomeTextE = document.getElementById("welcomeH");
+let loginWrapperE = document.getElementById("loginWrapper");
+
+addEventListener("DOMContentLoaded", (event) => {
+    console.log("DomContentLoaded");
+    const loginWrapperE_width = parseFloat(getComputedStyle(loginWrapperE).width)
+    if (loginWrapperE_width<=365) {
+        welcomeTextE.classList.remove("noWrap");
+        console.log("Changed welcomeH to wrap");
+    } else {
+        welcomeTextE.classList.add("noWrap");
+        console.log("Changed welcomeH to no wrap");
+    }
+})
+
+
+window.addEventListener('resize', (event)=>{
+    console.log("windowResized");
+    const loginWrapperE_width = parseFloat(getComputedStyle(loginWrapperE).width)
+    if (loginWrapperE_width<=365) {
+        welcomeTextE.classList.remove("noWrap");
+        console.log("Changed welcomeH to wrap");
+    } else {
+        welcomeTextE.classList.add("noWrap");
+        console.log("Changed welcomeH to no wrap");
+    }
+})
