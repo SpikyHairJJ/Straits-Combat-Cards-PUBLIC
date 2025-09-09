d093ed1b6c98dc9399331b4d318e9a709c48016e
Fayyadh03 <myspg0291@penangsisgroup.edu.my>
2025-09-09 15:01:06 +0800

    changed console message

--- static/js/login.js
+++ static/js/login.js
@@ -22,6 +22,6 @@ window.addEventListener('resize', (event)=>{
         console.log("Changed welcomeH to wrap");
     } else {
         welcomeTextE.classList.add("noWrap");
-        console.log("Changed welcomeH to no wrap");
+        console.log("i am flipping stupid");
     }
 })
